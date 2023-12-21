828b6633d971bbda640e5873ee51abeacb38bab3
Kristof Dombi <dombi.kristof@gmail.com>
2023-12-21 12:36:33 +0100

    chore(kinsta): add splash screen

--- src/main/java/com/kinsta/helloworld/App.java
+++ src/main/java/com/kinsta/helloworld/App.java
@@ -3,6 +3,9 @@ package com.kinsta.helloworld;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -13,7 +16,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Integer port = Integer.parseInt(
-          Optional.ofNullable(System.getenv("PORT")).orElse("8080")
+                Optional.ofNullable(System.getenv("PORT")).orElse("8080")
         );
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/", new MyHandler());
@@ -24,11 +27,26 @@ public class App {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            // Get the path to the HTML file in the root folder
+            Path filePath = Paths.get("index.html");
+
+            if (Files.exists(filePath) && Files.isReadable(filePath)) {
+                // Read the content of the HTML file
+                byte[] fileContent = Files.readAllBytes(filePath);
+
+                // Set the response headers
+                t.sendResponseHeaders(200, fileContent.length);
+                OutputStream os = t.getResponseBody();
+                os.write(fileContent);
+                os.close();
+            } else {
+                // If the HTML file is not found or not readable, send a 404 response
+                String response = "Hello World";
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
         }
     }
 
